15f8f3c7f0672d62c7fee474b01b2a8d7b5a1e57
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-08-25 00:23:37 -0300

    Primeros avances del entregable

--- primerEntregable/actions.js
+++ primerEntregable/actions.js
@@ -1,6 +1,32 @@
 let ctx = document.getElementById("canvas").getContext("2d");
-
 let image = new Image();
+let imageData;
+
+//Inicializacion de Sliders
+let brightnessSlider = document.getElementById("brightness");
+let brightnessOutput = document.getElementById("bvalue");
+brightnessOutput.innerHTML = brightnessSlider.value;
+
+let saturationSlider = document.getElementById("saturation");
+let saturationOutput = document.getElementById("svalue");
+saturationOutput.innerHTML = saturationSlider.value;
+
+brightnessSlider.oninput = function() {
+    brightnessOutput.innerHTML = this.value;
+}
+
+saturationSlider.oninput = function() {
+    saturationOutput.innerHTML = this.value;
+}
+
+//Lectura de Imagenes
+
+image.src = "https://i2.bssl.es/porconocer/2017/10/EmpireState.jpg";
+image.onload = function(){
+    ctx.drawImage(image,0,0);
+    imageData = ctx.getImageData(0,0,this.width,this.height);
+    ctx.putImageData(imageData, 0, 0);
+}
 
 function getCollors(imageData,x,y){
     let index = (x+y*imageData.width) * 4;
@@ -12,6 +38,7 @@ function getCollors(imageData,x,y){
 }
 
 function applyBlackAndWhiteFilter(imageData){
+    console.log(imageData.data);
     for (var i = 0; i < imageData.data.length; i+=4) {
         let promedio = ((imageData.data[i]+imageData.data[i+1]+imageData.data[i+2])/3);
         promedio = Math.floor(promedio);
@@ -21,15 +48,26 @@ function applyBlackAndWhiteFilter(imageData){
     }
 }
 
-function increaseBrigthness(imageData,ammount){
+function increaseBrigthness(imageData,amount){
+    console.log(imageData.data);
     for (var i = 0; i < imageData.data.length; i+=4) {
-        imageData.data[i]       += ammount;
-        imageData.data[i + 1]   += ammount;
-        imageData.data[i + 2]   += ammount;
+        imageData.data[i]       += amount;
+        imageData.data[i + 1]   += amount;
+        imageData.data[i + 2]   += amount;
+    }
+}
+
+function increaseSaturation(imageData,amount){
+    console.log(imageData.data);
+    for (var i = 0; i < imageData.data.length; i+=4) {
+        imageData.data[i]       += amount;
+        imageData.data[i + 1]   += amount;
+        imageData.data[i + 2]   += amount;
     }
 }
 
 function binarizacion(imageData){
+    console.log(imageData.data);
     for (var i = 0; i < imageData.data.length; i+=4) {
         imageData.data[i]       = (imageData.data[i]/2 > 127 )? 255 : 0;
         imageData.data[i + 1]   = (imageData.data[i]/2 > 127 )? 255 : 0;
@@ -38,6 +76,7 @@ function binarizacion(imageData){
 }
 
 function invert(imageData){
+    console.log(imageData.data);
     for (var i = 0; i < imageData.data.length; i+=4) {
         imageData.data[i]       =  255 - imageData.data[i]   ;
         imageData.data[i + 1]   =  255 - imageData.data[i+1] ;
@@ -45,10 +84,99 @@ function invert(imageData){
     }
 }
 
-image.src = "https://i2.bssl.es/porconocer/2017/10/EmpireState.jpg";
-image.onload = function(){
-        ctx.drawImage(image,0,0);
-        let imageData = ctx.getImageData(0,0,this.width,this.height);
-        invert(imageData);
-        ctx.putImageData(imageData, 0, 0);
+function SaveImage(){
+    console.log('saveFile');
+    let canvas = document.getElementById("canvas");
+    Canvas2Image.saveAsPNG(canvas);
 }
+
+function loadFile(){
+    let file    = document.querySelector('input[type=file]').files[0];
+    let reader  = new FileReader();
+
+    reader.onloadend = function () {
+        image.src = reader.result;
+        image.onload = function(){
+            ctx.drawImage(image,0,0);
+            imageData = ctx.getImageData(0,0,this.width,this.height);
+            ctx.putImageData(imageData, 0, 0);
+        }
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        image.src = "";
+    }
+}
+
+function setPixel(imageData, x, y, r, g, b, a) {
+    index = (x + y * imageData.width) * 4;
+    imageData.data[index + 0] = r;
+    imageData.data[index + 1] = g;
+    imageData.data[index + 2] = b;
+    imageData.data[index + 3] = a;
+}
+
+//EVENTOS DE BOTONES
+document.getElementById("pencil").addEventListener("click",function(event){
+    document.getElementById("canvasContainer").classList.add("pencil");
+    document.getElementById("canvas").addEventListener("mousemove",function(event){
+        if (event.buttons == 1){
+            var cX = event.layerX;
+            var cY = event.layerY;
+            setPixel(imageData,cX,cY,255,0,0,255);
+            ctx.putImageData(imageData,0,0);
+        }
+    })
+});
+
+document.getElementById("eraser").addEventListener("click",function(event){
+    document.getElementById("canvas").classList.add("pencil");
+    document.getElementById("canvas").addEventListener("mousemove",function(event){
+        if (event.buttons == 1){
+            var cX = event.layerX;
+            var cY = event.layerY;
+            setPixel(imageData,cX,cY,0,0,0,255);
+            ctx.putImageData(imageData,0,0);
+        }
+    })
+})
+
+
+document.getElementById("blackWhiteFilter").addEventListener("click",function(event){
+    applyBlackAndWhiteFilter(imageData);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+document.getElementById("invertFilter").addEventListener("click",function(event){
+    invert(imageData);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+document.getElementById("binaryFilter").addEventListener("click",function(event){
+    binarizacion(imageData);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+document.getElementById("brightness").addEventListener("input",function(event){
+    let amount = this.value;
+    increaseBrigthness(imageData,amount);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+document.getElementById("saturation").addEventListener("input",function(event){
+    let amount = this.value;
+    increaseSaturation(imageData,amount);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+document.getElementById("upload").addEventListener("change",function(event){
+    loadFile();
+});
+
+document.getElementById("saveFile").addEventListener("click",function(event){
+    SaveImage();
+});
+
+
