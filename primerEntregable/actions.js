b3000d83ed296d1e7cfd9af1319d5c07d6bb8be4
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-08-29 16:47:13 -0300

    ultimos cambios

--- primerEntregable/actions.js
+++ primerEntregable/actions.js
@@ -1,9 +1,12 @@
-let ctx = document.getElementById("canvas").getContext("2d");
+let canvas = document.getElementById("canvas").getContext("2d");
+let canvasContainer = document.getElementById("canvas");
 let image = new Image();
-let imageData = ctx.getImageData(0,0,1150,700);
+let imageData = canvas.getImageData(0,0,1150,700);
 let rgb = [0,0,0];
 let strokeSize = 5;
 let eraser = false;
+let canvasWidth = 1150;
+let canvasHeight = 700;
 
 //Inicializacion de Sliders
 let brightnessSlider = document.getElementById("brightness");
@@ -30,26 +33,57 @@ thicknessSlider.oninput = function() {
     thicknessOutput.innerHTML = this.value;
 }
 
-//Lectura de Imagenes
+function setWidth(width,height) {
+    canvasContainer.width = width;
+    canvasContainer.height = height;
+}
 
-// image.src = "https://i2.bssl.es/porconocer/2017/10/EmpireState.jpg";
-// image.onload = function(){
-//     ctx.drawImage(image,0,0);
-//     imageData = ctx.getImageData(0,0,this.width,this.height);
-//     ctx.putImageData(imageData, 0, 0);
-// }
-
-function getCollors(imageData,x,y){
-    let index = (x+y*imageData.width) * 4;
-    let r = imageData.data[index];
-    let g = imageData.data[index];
-    let b = imageData.data[index];
-    let  pixel = [r,g,b];
+function getPixel(imageData,x,y){
+    let  pixel = [0,0,0,0];
+    if (x > 0 || y > 0){
+        if (x < canvasWidth || y < canvasHeight ){
+            let index = (x+y*canvasWidth) * 4;
+            let r = imageData.data[index];
+            let g = imageData.data[index];
+            let b = imageData.data[index];
+            let a = imageData.data[index];
+            pixel = [r,g,b,a];
+        }
+    }
     return pixel;
 }
 
+function sumarPixeles(val,aux) {
+    val[0] += aux[0];
+    val[1] += aux[1];
+    val[2] += aux[2];
+    val[3] += aux[3];
+    return val;
+}
+
+function gussianBlur(radio) {
+
+    for (i=0;i<canvasHeight;i++){
+        for (j=0;j<canvasWidth;j++){
+            // Recorrido normal de matriz
+            let val = [0,0,0,0];
+            for (k=0;k<radio;k++){
+                //Recorrido de nodos dependiendo del radio
+                let aux = getPixel(imageData,x+k,y+k);
+                val = sumarPixeles(val,aux);
+                //Procesar el valor
+            }
+            aux = [0,0,0,0];
+            let r = val[0]/radio;
+            let g = val[1]/radio;
+            let b = val[2]/radio;
+            let a = val[3]/radio;
+            setPixel(imageData, x, y, r, g, b, a);
+        }
+    }
+}
+
 function applyBlackAndWhiteFilter(imageData){
-    console.log(imageData.data);
     for (let i = 0; i < imageData.data.length; i+=4) {
         let promedio = ((imageData.data[i]+imageData.data[i+1]+imageData.data[i+2])/3);
         promedio = Math.floor(promedio);
@@ -60,7 +94,6 @@ function applyBlackAndWhiteFilter(imageData){
 }
 
 function increaseBrigthness(imageData,amount){
-    console.log(imageData.data);
     for (let i = 0; i < imageData.data.length; i+=4) {
         imageData.data[i]       += amount;
         imageData.data[i + 1]   += amount;
@@ -69,16 +102,14 @@ function increaseBrigthness(imageData,amount){
 }
 
 function increaseSaturation(imageData,amount){
-    console.log(imageData.data);
     for (let i = 0; i < imageData.data.length; i+=4) {
-        imageData.data[i]       += amount;
-        imageData.data[i + 1]   += amount;
-        imageData.data[i + 2]   += amount;
+        imageData.data[i]       = imageData.data[i] + amount;
+        imageData.data[i + 1]   = imageData.data[i+1] + amount;
+        imageData.data[i + 2]   = imageData.data[i+2] + amount;
     }
 }
 
 function binarizacion(imageData){
-    console.log(imageData.data);
     for (let i = 0; i < imageData.data.length; i+=4) {
         imageData.data[i]       = (imageData.data[i]/2 > 127 )? 255 : 0;
         imageData.data[i + 1]   = (imageData.data[i]/2 > 127 )? 255 : 0;
@@ -98,9 +129,17 @@ function invert(imageData){
 //Funciones para guardado y subida de archivos
 
 function SaveImage(){
-    console.log('saveFile');
-    let canvas = document.getElementById("canvas");
-    Canvas2Image.saveAsPNG(canvas);
+
+    let link = window.document.createElement( 'a' ),
+    url = canvasContainer.toDataURL(), filename = 'screenshot.jpg';
+
+    link.setAttribute( 'href', url );
+    link.setAttribute( 'download', filename );
+    link.style.visibility = 'hidden';
+    window.document.body.appendChild( link );
+    link.click();
+    window.document.body.removeChild( link );
+
 }
 
 function loadFile(){
@@ -111,12 +150,14 @@ function loadFile(){
     reader.onloadend = function () {
         image.src = reader.result;
         image.onload = function(){
-
-            ctx.drawImage(image, image.width/2, 0)
-            imageData = ctx.getImageData(0,0,this.width,this.height);
-            ctx.putImageData(image, image.width/2, 0);
+            canvasHeight = image.height;
+            canvasWidth = image.width;
+            setWidth(canvasHeight,canvasWidth);
+            canvas.drawImage(image, 0, 0);
+            imageData = canvas.getImageData(0,0,canvasHeight,canvasHeight);
+            canvas.putImageData(imageData, 0, 0);
         }
-    }
+    };
 
     if (file) {
         reader.readAsDataURL(file); //reads the data as a URL
@@ -126,9 +167,8 @@ function loadFile(){
 }
 
 //function to paint
-
 function setPixel(imageData, x, y, r, g, b, a) {
-    index = (x + y * imageData.width) * 4;
+    index = (x + y * canvasWidth) * 4;
     imageData.data[index + 0] = r;
     imageData.data[index + 1] = g;
     imageData.data[index + 2] = b;
@@ -145,9 +185,9 @@ function pintar(imageData, x, y, r, g, b, a){
 
 function colorear(rgb,event){
     let cX = event.layerX;
-    let cY = event.layerY;
+    let cY = event.layerY+25; //Ajuste de posicion del pintado
     pintar(imageData,cX,cY,rgb[0],rgb[1],rgb[2],255,10);
-    ctx.putImageData(imageData,0,0);
+    canvas.putImageData(imageData,0,0);
 }
 
 function getSelectedColor(){
@@ -155,13 +195,13 @@ function getSelectedColor(){
 }
 
 function cleanCanvas(){
-    imageData = ctx.createImageData(width=1150, height=700);
-    for(x=0; x<width; x++){
-        for(y=0; y<height; y++){
+    imageData = canvas.createImageData(canvasWidth, canvasHeight);
+    for(x=0; x<canvasWidth; x++){
+        for(y=0; y<canvasHeight; y++){
             setPixel(imageData, x, y, 255, 255, 255, 255);
         }
     }
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 }
 
 //EVENTOS DE BOTONES
@@ -189,7 +229,6 @@ document.getElementById("pencil").addEventListener("click",function() {
 
 document.getElementById("eraser").addEventListener("click",function() {
     eraser = true;
-    console.log(eraser);
     document.getElementById("canvas").classList.remove("pencil");
     document.getElementById("canvas").classList.add("eraser");
 
@@ -205,54 +244,45 @@ document.getElementById("canvas").addEventListener("mousemove",function(event){
     }
 });
 
-document.getElementById("blackWhiteFilter").addEventListener("click",function(event){
+document.getElementById("blackWhiteFilter").addEventListener("click",function(){
     applyBlackAndWhiteFilter(imageData);
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 });
 
-document.getElementById("invertFilter").addEventListener("click",function(event){
+document.getElementById("invertFilter").addEventListener("click",function(){
     invert(imageData);
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 });
 
-document.getElementById("binaryFilter").addEventListener("click",function(event){
+document.getElementById("binaryFilter").addEventListener("click",function(){
     binarizacion(imageData);
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 });
 
-document.getElementById("brightness").addEventListener("input",function(event){
-    let amount = this.value;
-    console.log(amount);
+document.getElementById("brightness").addEventListener("input",function(){
+    let amount = parseInt(this.value);
     increaseBrigthness(imageData,amount);
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 });
 
-document.getElementById("saturation").addEventListener("input",function(event){
-    let amount = this.value;
-    console.log(amount);
+document.getElementById("saturation").addEventListener("input",function(){
+    let amount = parseInt(this.value);
     increaseSaturation(imageData,amount);
-    ctx.putImageData(imageData, 0, 0);
+    canvas.putImageData(imageData, 0, 0);
 });
 
-document.getElementById("saturation").addEventListener("input",function(event){
-    let amount = this.value;
-    console.log(amount);
-    increaseSaturation(imageData,amount);
-    ctx.putImageData(imageData, 0, 0);
-});
-
-document.getElementById("pencilThickness").addEventListener("input",function(event){
+document.getElementById("pencilThickness").addEventListener("input",function(){
     strokeSize = this.value;
 });
 
-document.getElementById("gussianBlur").addEventListener("click",function(event){
-    alert('hi under development');
+document.getElementById("gussianBlur").addEventListener("click",function(){
+    gussianBlur(38);
 });
 
-document.getElementById("upload").addEventListener("change",function(event){
+document.getElementById("upload").addEventListener("change",function(){
     loadFile();
 });
 
-document.getElementById("saveFile").addEventListener("click",function(event){
+document.getElementById("saveFile").addEventListener("click",function(){
     SaveImage();
 });
